44b7bd14d77ae3aaf13c18061d5172e2ee0020e2
pushkarra <pushkarpuru97199@gmail.com>
2025-01-07 12:16:57 +0530

    meedhansh half changes

--- models/schema.js
+++ models/schema.js
@@ -33,11 +33,7 @@ const productSchema = new mongoose.Schema({
         type: [String],
         required:false,
     },
-    
-    lumen: {
-        type: [String],
-        required: false,
-    },
+
     size: {
         required: false,
         type: [String],
@@ -46,50 +42,23 @@ const productSchema = new mongoose.Schema({
         type: [String],
         required: false,
     },
-    features: {
-        type: [String],
-        required: false,
-    },
+    
     featured: {
         type: Boolean,
         default: false,
     },
 
-
-    voltage1: {
-        type: String,
-    },
-    voltage2: {
-        type: String,
-    },
-    current: {
-        type: String,
-    },
-    pf: {
-        type: String,
-    },
-    surge: {
-        type: String,
-    },
     typeo: {
         type: String,
     },
     dimension: {
         type: String,
     },
-    color:{
-        type: String,
-    },
+    
     subcategory:{
         type: String,
     },
-    lengthy: {
-        required: false,
-        type: [String],
-    },
-    source: {
-        type: [String],
-    }
+    
 });
 
 
