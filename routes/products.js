6a3efd8c525b6719f3fbd0a57d13db19f0621780
pushkarra <pushkarpuru97199@gmail.com>
2025-01-09 13:09:05 +0530

    single id for pdp

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,21 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const {getAllProducts, getAllProductsTesting,} =  require("../controllers/products");
+
+// router.route('/').get(getAllProducts);
+// router.route("/testing").get(getAllProductsTesting);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 
-const {getAllProducts, getAllProductsTesting,} =  require("../controllers/products");
+const { getAllProducts, getAllProductsTesting, getProductById } = require("../controllers/products");
 
 router.route('/').get(getAllProducts);
-router.route("/testing").get(getAllProductsTesting);
+router.route('/testing').get(getAllProductsTesting);
+router.route('/:id').get(getProductById); // New route for fetching a product by ID
 
 module.exports = router;
+
